a40b493919941267dfc9e4ad62b1924cf1d00bbf
Innsbluck <innsbluck@gmail.com>
2026-01-21 16:36:36 +0900

    v0.2.1

--- src/thumbnail/index.ts
+++ src/thumbnail/index.ts
@@ -1,4 +1,4 @@
-export { LayerThumbnail } from './LayerThumbnail';
-export type { LayerThumbnailOptions } from './LayerThumbnail';
 export { FrascoThumbnail } from './FrascoThumbnail';
 export type { FrascoThumbnailOptions } from './FrascoThumbnail';
+export { LayerThumbnail } from './LayerThumbnail';
+export type { LayerThumbnailOptions } from './LayerThumbnail';
